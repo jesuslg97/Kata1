0cba6ee0c9d67e9531ed0a09d744227ad64a2af9
jelop <jelop@10.16.10.3>
2018-09-21 11:43:50 +0100

    Kata1 version 2

--- Kata1/src/kata1/Kata1.java
+++ Kata1/src/kata1/Kata1.java
@@ -8,8 +8,8 @@ public class Kata1 {
     public static void main(String[] args) {
         Calendar date;
         date = GregorianCalendar.getInstance();
-        date.set(1949,9,24);
-        Person person = new Person("Blas", date);
+        date.set(1997,6,21);
+        Person person = new Person("Jesus", date);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }   
 }
